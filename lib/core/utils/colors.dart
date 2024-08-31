02dc35691d50b64c674e8d03895736df468cf598
samaa <samaasoltan6@gmail.com>
2024-09-01 01:24:14 +0300

    add new texts, colors, and text styles

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryBlackColor       = Color(0xFF222222);
+  static const Color primaryGreyColor        = Color(0xFFF9F8F8);
+  static const Color primaryMediumGreyColor  = Color(0xFF9B9B9B);
+  static const Color primaryWhiteColor       = Color(0xFFFFFFFF);
+  static const Color primaryRedColor         = Color(0xFFDB3022);
+}
---
+++ lib/core/utils/text_styles.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'colors.dart';
+
+class AppTextStyles {
+  static TextStyle fontBlack30Bold = TextStyle(fontSize: 30.sp, fontWeight: FontWeight.bold, color: AppColors.primaryBlackColor);
+  static TextStyle fontBlack14W400 = TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w400, color: AppColors.primaryBlackColor);
+
+  static TextStyle fontGrey12W400  = TextStyle(fontSize: 12.sp, color: AppColors.primaryMediumGreyColor);
+  static TextStyle fontGrey14W400  = TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w400, color: AppColors.primaryMediumGreyColor);
+
+  static TextStyle fontWhite14W400 = TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w400, color: AppColors.primaryWhiteColor);
+
+}
